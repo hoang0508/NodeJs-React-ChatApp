ca75d72576e2485acb3081f633742c840602c0bd
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-11-11 21:12:41 +0700

    setup-h1

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/profile/:username" element={<Profile />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
