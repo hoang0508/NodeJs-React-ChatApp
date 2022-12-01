c19a00cf9d25a0411f1c50152842a004d2510398
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-12-01 21:53:18 +0700

    hoang-1

---
+++ .history/client/src/pages/login/Login_20221121232257.jsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import "./login.css";
+
+export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const handleSubmitForm = (e) => {
+      e.preventDefault();
+      console.log(email.current)
+  }
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">HH social media</h3>
+          <span className="loginDesc">
+            Kết nối với bạn bè và thế giới xung quanh bạn trên mạng xã hội.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleSubmitForm}>
+            <input placeholder="Email" type="email" className="loginInput" ref={email}/>
+            <input placeholder="Password" type="password" className="loginInput" ref={password} />
+            <button className="loginButton">Đăng nhập</button>
+            <span className="loginForgot">Quên mật khẩu?</span>
+            <button className="loginRegisterButton">Tạo tài khoản mới!!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/client/src/pages/profile/Profile_20221113204000.jsx
@@ -0,0 +1,52 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Profile() {
+  const [user, setUser] = useState({});
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=moon`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src={`${PF}post/3.jpeg`}
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src={`${PF}person/7.jpeg`}
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">{user?.username}</h4>
+              <span className="profileInfoDesc">{user?.desc}</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed username="moon" />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
