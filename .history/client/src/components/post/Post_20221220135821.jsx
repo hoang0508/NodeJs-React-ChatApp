413d878d09ef4a16e66793c0730d8662ecd7a758
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-12-20 15:00:15 +0700

    fix bug post done hoang8

---
+++ .history/client/src/components/post/Post_20221220135821.jsx
@@ -0,0 +1,328 @@
+import "./post.scss";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { format } from "timeago.js";
+import { AuthContext } from "../../context/AuthContext";
+import {
+  BsFillFileEarmarkPostFill,
+  BsFillTrashFill,
+  BsThreeDots,
+} from "react-icons/bs";
+import Swal from "sweetalert2";
+import "sweetalert2/src/sweetalert2.scss";
+import ModalUpdate from "../modal/ModalUpdate";
+import Input from "../input/Input";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import useValueToggle from "../../hooks/useValueToggle";
+
+export default function Post({ post }) {
+  const [like, setLike] = useState(post.likes.length);
+  const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+  const [friends, setFriends] = useState([]);
+  const [textLike, setTextLike] = useState("");
+  const [dataUserOne, setDataUserOne] = useState(undefined);
+  const { handleValueToggle, valueToggle } = useValueToggle();
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const {
+    user: currentUser,
+    setPosts,
+    posts,
+    showPostUpdate,
+    setShowPostUpdate,
+    file,
+    setFile,
+  } = useContext(AuthContext);
+
+  // useForm hook
+  const { handleSubmit, control, reset } = useForm({
+    mode: "onSubmit",
+    defaultValues: {
+      desc: "",
+    },
+  });
+
+  useEffect(() => {
+    setIsLiked(post?.like?.includes(currentUser?._id));
+  }, [post?.like, currentUser?._id]);
+
+  const Likehandle = () => {
+    try {
+      axios.put(`/posts/${post._id}/like`, { userId: currentUser._id });
+    } catch (error) {
+      console.log(error);
+    }
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?userId=${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.userId]);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const friendList = await axios.get("/users/friends/" + user._id);
+        setFriends(friendList.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriends();
+  }, [user]);
+
+  const userLike =
+    friends &&
+    friends.length > 0 &&
+    friends
+      .slice(0, 3)
+      .map((item) => item?.username)
+      .join(",");
+
+  useEffect(() => {
+    if (like === 0) {
+      setTextLike("Chưa có lượt thích!");
+    } else if (like < 3) {
+      setTextLike(`${like} lượt thích`);
+    } else if (like >= 3) {
+      setTextLike(`${userLike} và ${friends.length} người khác`);
+    }
+  }, [friends.length, like, userLike]);
+
+  // Delete Post
+  const handleDeletePost = async (postId) => {
+    Swal.fire({
+      title: "Bạn có chắc chắn xóa?",
+      text: "Nội dung này sẽ xóa bài viết của bạn",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Tôi đồng ý!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await axios.delete(`/posts/${postId}`, {
+          data: {
+            userId: post?.userId,
+          },
+        });
+        const postData =
+          posts &&
+          posts.length > 0 &&
+          posts.filter((item) => item._id !== postId);
+        setPosts(postData);
+        Swal.fire("Xóa bài viết!", "Đã xóa bài viết.", "success");
+      }
+    });
+  };
+
+  // Update Post
+
+  const handleClickShowUpdate = (postItem) => {
+    setShowPostUpdate(postItem);
+  };
+
+  useEffect(() => {
+    reset({
+      desc: showPostUpdate?.desc,
+    });
+  }, [showPostUpdate, reset]);
+
+  const handleCloseUpdate = () => {
+    setShowPostUpdate(false);
+    reset({
+      desc: "",
+    });
+  };
+
+  const handleUpdateShare = async (values) => {
+    const updatePost = {
+      userId: user._id,
+      desc: values.desc,
+      img: showPostUpdate.img,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      updatePost.img = fileName;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      await axios.put(`/posts/${showPostUpdate?._id}`, updatePost);
+      const updateDataPost =
+        posts &&
+        posts.map((item) => {
+          if (item._id === showPostUpdate._id) {
+            return {
+              ...item,
+              ...updatePost,
+            };
+          }
+          return item;
+        });
+      setPosts(updateDataPost);
+      setFile(null);
+      reset({
+        desc: "",
+      });
+      setShowPostUpdate("");
+      toast.success("Lưu bài viết thành công!!");
+    } catch (error) {
+      console.log(error);
+      toast.error("Lưu bài thất bại!!");
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const resUser = await axios.get(`/users?userId=${currentUser?._id}`);
+      setDataUserOne(resUser?.data);
+    };
+    fetchUserData();
+  }, [currentUser?._id]);
+
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <Link to={`profile/${user?.username}`}>
+              <div className="postProfileImg">
+                <img
+                  src={
+                    user?.profilePicture
+                      ? PF + user?.profilePicture
+                      : PF + "person/noAvatar.png"
+                  }
+                  alt=""
+                />
+              </div>
+            </Link>
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
+          </div>
+          <div className="postTopRight">
+            <span className="postTopRight-dots">
+              <BsThreeDots />
+            </span>
+            <div className="postUpdate">
+              <div
+                className="postUpdate-info"
+                onClick={() => handleClickShowUpdate(post)}
+              >
+                <span className="postUpdate-info--icon">
+                  <BsFillFileEarmarkPostFill />
+                </span>
+                <span className="postUpdate-info--text">
+                  Chỉnh sửa bài viết
+                </span>
+              </div>
+              <div
+                className="postUpdate-info"
+                onClick={() => handleDeletePost(post?._id)}
+              >
+                <span className="postUpdate-info--icon">
+                  <BsFillTrashFill />
+                </span>
+                <span className="postUpdate-info--text">Xóa bài viết</span>
+              </div>
+            </div>
+          </div>
+          {!!showPostUpdate && (
+            <ModalUpdate
+              heading="Sửa bài viết"
+              closeModal={() => handleCloseUpdate()}
+              textBtn="Lưu bài viết"
+              handleSubmit={handleSubmit(handleUpdateShare)}
+            >
+              <>
+                <div className="modal-share--info">
+                  <div className="modal-share--avatar">
+                    <img
+                      src={
+                        user.profilePicture
+                          ? PF + user.profilePicture
+                          : PF + "person/noAvatar.png"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <span className="modal-share--name">
+                    {dataUserOne?.username ? dataUserOne?.username : ""}
+                  </span>
+                </div>
+                <Input
+                  placeholder={`${
+                    dataUserOne?.username ? dataUserOne?.username : ""
+                  } ơi, bạn đang nghĩ gì thế?`}
+                  className="modal-share--input"
+                  name="desc"
+                  control={control}
+                />
+                <div className={"modal-share--upload"}>
+                  {file || showPostUpdate.img ? (
+                    <img
+                      src={
+                        file
+                          ? URL.createObjectURL(file)
+                          : PF + showPostUpdate.img
+                      }
+                      alt="#"
+                    />
+                  ) : (
+                    ""
+                  )}
+                  {file && (
+                    <span
+                      className="modal-share--upload-remove"
+                      onClick={() => setFile(null)}
+                    >
+                      <BsFillTrashFill />
+                    </span>
+                  )}
+                </div>
+              </>
+            </ModalUpdate>
+          )}
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={PF + post.img} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img
+              className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={Likehandle}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={Likehandle}
+              alt=""
+            />
+            <span className="postLikeCounter">{textLike}</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/client/src/components/topbar/Topbar_20221219143935.jsx
@@ -0,0 +1,68 @@
+import "./topbar.scss";
+import { Search } from "@material-ui/icons";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { IoNotificationsSharp } from "react-icons/io5";
+import { toast } from "react-toastify";
+
+export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const navigate = useNavigate();
+
+  const handleProfileLogout = () => {
+    localStorage.removeItem("user");
+    toast.success("Đăng xuất thành công!!");
+    navigate("/login");
+  };
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to={"/"}>
+          <span className="logo">HH social media</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <FaFacebookMessenger />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <IoNotificationsSharp />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <Link to={`/profile/${user.username}`}>
+          {" "}
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+        <button
+          className="profile-logout"
+          onClick={() => handleProfileLogout()}
+        >
+          Đăng xuất
+        </button>
+      </div>
+    </div>
+  );
+}
